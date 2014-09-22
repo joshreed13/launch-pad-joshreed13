c5974452c85b6ca97d0281774d570571bee31ca3
Josh Reed <jlrdkc@mst.edu>
2014-09-21 22:26:27 -0500

    Set up DataContext for ConsoleView.xaml

--- LaunchPadJoshreed13/LaunchPadJoshreed13/Views/ConsoleView.xaml.cs
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/Views/ConsoleView.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LaunchPadJoshreed13.ViewModels;
 
 namespace LaunchPadJoshreed13.Views
 {
@@ -23,6 +24,7 @@ namespace LaunchPadJoshreed13.Views
         public ConsoleView()
         {
             InitializeComponent();
+            this.DataContext = new ConsoleViewModel();
         }
     }
 }
