0f239d094845f54398efe2d4258175752dcab622
Josh Reed <jlrdkc@mst.edu>
2014-09-21 22:25:15 -0500

    Set up DataContext for MainWindow.xaml

--- LaunchPadJoshreed13/LaunchPadJoshreed13/MainWindow.xaml.cs
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LaunchPadJoshreed13.ViewModels;
 
 namespace LaunchPadJoshreed13
 {
@@ -23,6 +24,7 @@ namespace LaunchPadJoshreed13
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new MainWindowViewModel();
         }
     }
 }
