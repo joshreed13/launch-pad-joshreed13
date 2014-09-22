981353d653008921e9c74bea39896396f32c121d
Josh Reed <jlrdkc@mst.edu>
2014-09-21 22:32:31 -0500

    Set up model for MainWindowViewModel

--- LaunchPadJoshreed13/LaunchPadJoshreed13/ViewModels/MainWindowViewModel.cs
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/ViewModels/MainWindowViewModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LaunchPadJoshreed13.Models;
 
 namespace LaunchPadJoshreed13.ViewModels
 {
     class MainWindowViewModel
     {
+        private MainWindowModel Model;
+
+        public MainWindowViewModel()
+        {
+            Model = new MainWindowModel();
+        }
     }
 }
