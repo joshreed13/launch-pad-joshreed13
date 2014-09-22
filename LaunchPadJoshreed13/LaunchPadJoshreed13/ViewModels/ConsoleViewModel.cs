912a182ad4cc9724741d9480b401a67edfa0a31c
Josh Reed <jlrdkc@mst.edu>
2014-09-21 22:31:21 -0500

    Set up model for ConsoleViewModel

--- LaunchPadJoshreed13/LaunchPadJoshreed13/ViewModels/ConsoleViewModel.cs
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/ViewModels/ConsoleViewModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LaunchPadJoshreed13.Models;
 
 namespace LaunchPadJoshreed13.ViewModels
 {
     class ConsoleViewModel
     {
+        private ConsoleModel Model;
+
+        public ConsoleViewModel()
+        {
+            Model = new ConsoleModel();
+        }
     }
 }
