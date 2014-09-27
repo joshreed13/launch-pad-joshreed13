6ebfd25e11609c5bf9c0a87e53870ac2376a5a7c
Josh Reed <jlrdkc@mst.edu>
2014-09-27 12:36:08 -0500

    Added RoverView View

---
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/Views/RoverView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace LaunchPadJoshreed13.Views
+{
+    /// <summary>
+    /// Interaction logic for RoverView.xaml
+    /// </summary>
+    public partial class RoverView : UserControl
+    {
+        public RoverView()
+        {
+            InitializeComponent();
+        }
+    }
+}
