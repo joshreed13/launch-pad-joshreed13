21c66d1c78298d2233d94fb71dc009b6870d4f8a
Josh Reed <jlrdkc@mst.edu>
2014-09-27 12:51:18 -0500

    Added Properties to RoverViewModel

--- LaunchPadJoshreed13/LaunchPadJoshreed13/ViewModels/RoverViewModel.cs
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/ViewModels/RoverViewModel.cs
@@ -7,6 +7,58 @@ namespace LaunchPadJoshreed13.ViewModels
     {
         private RoverModel Model;
 
+        public string RoverName
+        {
+            get
+            {
+                return Model.roverName;
+            }
+            set
+            {
+                Model.roverName = value;
+                NotifyOfPropertyChange(() => RoverName);
+            }
+        }
+
+        public int Speed
+        {
+            get
+            {
+                return Model.speed;
+            }
+            set
+            {
+                Model.speed = value;
+                NotifyOfPropertyChange(() => Speed);
+            }
+        }
+
+        public double Temperature
+        {
+            get
+            {
+                return Model.temperature;
+            }
+            set
+            {
+                Model.temperature = value;
+                NotifyOfPropertyChange(() => Temperature);
+            }
+        }
+
+        public bool IsConnected
+        {
+            get
+            {
+                return Model.isConnected;
+            }
+            set
+            {
+                Model.isConnected = value;
+                NotifyOfPropertyChange(() => IsConnected);
+            }
+        }
+
         public RoverViewModel()
         {
             Model = new RoverModel();
