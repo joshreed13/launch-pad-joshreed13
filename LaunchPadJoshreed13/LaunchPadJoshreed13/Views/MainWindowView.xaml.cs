952982bd6b52f6056778765b08c646fcf4612429
Josh Reed <jlrdkc@mst.edu>
2014-09-26 19:52:17 -0500

    Set up Bootstrapper

--- LaunchPadJoshreed13/LaunchPadJoshreed13/Views/MainWindowView.xaml.cs
+++ LaunchPadJoshreed13/LaunchPadJoshreed13/Views/MainWindowView.xaml.cs
@@ -1,17 +1,16 @@
 ﻿using LaunchPadJoshreed13.ViewModels;
 using System.Windows;
 
-namespace LaunchPadJoshreed13
+namespace LaunchPadJoshreed13.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowView : Window
     {
-        public MainWindow()
+        public MainWindowView()
         {
             InitializeComponent();
-            this.DataContext = new MainWindowViewModel();
         }
     }
 }
